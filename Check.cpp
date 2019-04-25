88aea0a3eb65f83d2d91e15fb1025a13949a8edf
Max Base <MaxBaseCode@Gmail.Com>
2019-04-25 17:07:58 +0430

    Update Check.cpp

--- Check.cpp
+++ Check.cpp
@@ -3,7 +3,22 @@
 using std::cin;
 using std::cout;
 
+// Recursion
 int gcd(int a, int b) {
+	if(a == 0 || b == 0) {
+		return 0;
+	}
+	if(a == b) {
+		return a;
+	}
+	if(a > b) {
+		return gcd(a-b, b);
+	}
+	return gcd(a, b-a);
+}
+
+// Loop
+int gcd2(int a, int b) {
 	for(;;) {
 		if(a == 0) {
 			return b;
